ecb8289e6ecd7579cbbdbbe245fa5d0d960e8cf9
Ale Cuahuizo <alejandracuahuizo970@gmail.com>
2025-11-25 14:05:55 -0600

    Version 1

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Proyecto-app-Web
+
+Aplicación web sobre pingüinos.
+
+Tenemos una base de datos 'penguins.csv' que muestra los siguientes campos:
+
+- id
+- species: tres tipos de especies de pingüinos (Adelia, Barbijo o Gentoo).
+- island: Nombre de la isla donde se observó y vivió el pingüino.
+- bill_length_mm: Longitud del pico del pingüino en milímetros. 
+- bill_depth_mm: Profundidad del pico del pingüino en milímetros
+- flipper_length_mm: Longitud de la aleta del pingüino en milímetros
+- body_mass_g: Masa corporal del pingüino en gramos. 
+- sex: Género del pingüino (macho o hembra)
+- year: Año de observación.
+
+Se creó un app para visualizar gráficos relevantes.
---
+++ notebooks/app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+import plotly_express as px
+
+penguins_data = pd.read_csv('penguins.csv')
+
+# ----- Encabezado ----
+st.header('Información sobre especies de pingüinos')
+
+# ----- Botón para mostrar un histograma-----
+boton_hist = st.button('Construir un histograma')
+
+# Lógica para actiar el botón
+if boton_hist:
+    st.write(
+        'Histograma que muestra la frecuencia de la longitud de la aleta del pinguino en mm')
+
+    # Creación del histograma
+    fig = px.histogram(penguins_data, 'flipper_length_mm')
+
+    # Mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+
+# ----- Botón para mostrar un gráfico de dispersión ----
+boton_disp = st.button('Construir un diagrama de dispersión')
+
+if boton_disp:
+
+    # Creación del diagrama
+    diagrama = px.scatter(penguins_data, 'bill_length_mm', 'body_mass_g')
+
+    # Mostrar el diagrama
+    st.plotly_chart(diagrama, use_container_width=True)
